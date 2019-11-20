4005cacccc2c5b59f4219276b787ae7ba8901cb8
Mikhail Yudin <m.yudin@bpmonline.com>
2019-11-20 19:54:52 +0200

    NUnit setup

---
+++ src/Global.Configuration.Dal.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Global.Configuration.Dal.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Global.Configuration.Dal.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e2eb3a9c-22c3-453c-af6b-8b00dc86c462")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/Global.Configuration.Dal.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Global.Configuration.Dal.Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void AlwaysEqual()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
